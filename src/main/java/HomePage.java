e6b81d8d551a2f3ae7c5a1adaff00b357b478af0
Alexander Kurganov <sunburn2008@ya.ru>
2024-09-25 13:55:27 +0300

    First test complite

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -7,11 +7,35 @@ public class HomePage {
 
     private final WebDriver webDriver;
 
-    private final String url = "https://qa-scooter.praktikum-services.ru/";
+    public static final By accordionQuestion1 = By.xpath("//div[@id='accordion__heading-0']");
+    public static final By accordionQuestion2 = By.xpath("//div[@id='accordion__heading-1']");
+    public static final By accordionQuestion3 = By.xpath("//div[@id='accordion__heading-2']");
+    public static final By accordionQuestion4 = By.xpath("//div[@id='accordion__heading-3']");
+    public static final By accordionQuestion5 = By.xpath("//div[@id='accordion__heading-4']");
+    public static final By accordionQuestion6 = By.xpath("//div[@id='accordion__heading-5']");
+    public static final By accordionQuestion7 = By.xpath("//div[@id='accordion__heading-6']");
+    public static final By accordionQuestion8 = By.xpath("//div[@id='accordion__heading-7']");
 
+    public static final By accordionAnswer1 = By.xpath("//*[@id='accordion__panel-0']/p");
+//    public static final By accordionItemPanel2 = By.xpath("//div[@id='accordion__heading-1']");
+//    public static final By accordionItemPanel3 = By.xpath("//div[@id='accordion__heading-2']");
+//    public static final By accordionItemPanel4 = By.xpath("//div[@id='accordion__heading-3']");
+//    public static final By accordionItemPanel5 = By.xpath("//div[@id='accordion__heading-4']");
+//    public static final By accordionItemPanel6 = By.xpath("//div[@id='accordion__heading-5']");
+//    public static final By accordionItemPanel7 = By.xpath("//div[@id='accordion__heading-6']");
+//    public static final By accordionItemPanel8 = By.xpath("//div[@id='accordion__heading-7']");
+
+    public static final String EXPECTED_ANSWER_1 = "Сутки — 400 рублей. Оплата курьеру — наличными или картой.";
+    public static final String EXPECTED_ANSWER_2 = "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.";
+    public static final String EXPECTED_ANSWER_3 = "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.";
+    public static final String EXPECTED_ANSWER_4 = "Только начиная с завтрашнего дня. Но скоро станем расторопнее.";
+    public static final String EXPECTED_ANSWER_5 = "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.";
+    public static final String EXPECTED_ANSWER_6 = "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.";
+    public static final String EXPECTED_ANSWER_7 = "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.";
+    public static final String EXPECTED_ANSWER_8 = "Да, обязательно. Всем самокатов! И Москве, и Московской области.";
+
+    private final String url = "https://qa-scooter.praktikum-services.ru/";
     private final By accordionItemHeading = By.xpath("//*[@id=\"accordion__heading-0\"]");
-    private final By accordionItemPanel = By.xpath("//*[@id=\"accordion__panel-0\"]");
-    private final By accordionItemPanelText = By.xpath("//*[@id=\"accordion__panel-0\"]");
 
     HomePage(WebDriver webDriver) {
         this.webDriver = webDriver;
@@ -21,13 +45,17 @@ public class HomePage {
         webDriver.get(url);
     }
 
-    public void scrollToAccordion() {
-        WebElement element = webDriver.findElement(accordionItemHeading);
+    public void scrollToAccordion(By questionLocator) {
+        WebElement element = webDriver.findElement(questionLocator);
         ((JavascriptExecutor) webDriver).executeScript("arguments[0].scrollIntoView();", element);
     }
 
-    public void clickAccordionItem() {
-        webDriver.findElement(accordionItemHeading).click();
+    public void clickAccordionItem(By questionLocator) {
+        webDriver.findElement(questionLocator).click();
+    }
+
+    public String getActualAnswer(By answerLockator) {
+        return webDriver.findElement(answerLockator).getText();
     }
 
 }
--- src/test/java/ScooterTest.java
+++ src/test/java/ScooterTest.java
@@ -1,30 +1,61 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.Assert.assertEquals;
 
+@RunWith(Parameterized.class)
 public class ScooterTest {
     WebDriver webDriver;
+    private String expectedResult;
+    private By answerLocator;
+    private By questionLocator;
 
     @Before
     public void setup() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--no-sandbox", "--disable-dev-shm-usage");
-        webDriver = new ChromeDriver(options);
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--no-sandbox", "--disable-dev-shm-usage");
+        webDriver = new FirefoxDriver();
     }
+
+        public ScooterTest(String expectedResult, By answerLocator, By questionLocator) {
+        this.expectedResult = expectedResult;
+        this.answerLocator = answerLocator;
+        this.questionLocator = questionLocator;
+        }
+
+    @Parameterized.Parameters
+    public static Object[][] Results() {
+        return new Object[][] {
+                {HomePage.EXPECTED_ANSWER_1, HomePage.accordionAnswer1, HomePage.accordionQuestion1},
+//                {HomePage.EXPECTED_ANSWER_2, HomePage.accordionItemPanel2},
+//                {HomePage.EXPECTED_ANSWER_3, HomePage.accordionItemPanel3},
+//                {HomePage.EXPECTED_ANSWER_4, HomePage.accordionItemPanel4},
+//                {HomePage.EXPECTED_ANSWER_5, HomePage.accordionItemPanel5},
+//                {HomePage.EXPECTED_ANSWER_6, HomePage.accordionItemPanel6},
+//                {HomePage.EXPECTED_ANSWER_7, HomePage.accordionItemPanel7},
+//                {HomePage.EXPECTED_ANSWER_8, HomePage.accordionItemPanel8},
+        };
+    }
+
 
     @Test
     public void checkValueItemPanel() {
         HomePage homePage = new HomePage(webDriver);
 
         homePage.open();
-        homePage.scrollToAccordion();
-        homePage.clickAccordionItem();
+        homePage.scrollToAccordion(questionLocator);
+        homePage.clickAccordionItem(questionLocator);
+
+        assertEquals("Ты написал говно!", expectedResult, homePage.getActualAnswer(answerLocator));
+
     }
 
     @After
